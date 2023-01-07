418c382e6b731055aef2d0edf5fb8e6f7a03c708
gegaenea@yahoo.com <gegaenea@yahoo.com>
2023-01-07 13:26:50 +0300

    date and time tested

--- flight/views.py
+++ flight/views.py
@@ -3,10 +3,23 @@ from rest_framework import viewsets
 from .serializers import FlightSerializer, ReservationSerializer, StaffFlightSerializer
 from .models import Flight, Reservation
 from .permissions import IsStafforReadOnly
+from datetime import datetime, date
 # Create your views here.
 
 
 
+
+# class FlightView(viewsets.ModelViewSet):
+#     queryset = Flight.objects.all()
+#     serializer_class = FlightSerializer
+#     permission_classes = (IsStafforReadOnly,)
+    
+#     def get_serializer_class(self):
+#         serializer = super().get_serializer_class()
+        
+#         if self.request.user.is_staff:
+#             return StaffFlightSerializer
+#         return serializer
 
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
@@ -15,10 +28,26 @@ class FlightView(viewsets.ModelViewSet):
     
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
-        
         if self.request.user.is_staff:
             return StaffFlightSerializer
         return serializer
+    
+    def get_queryset(self):
+        now = datetime.now()
+        current_time = now.strftime('%H:%M:%S')
+        today = date.today()
+        
+        if self.request.user.is_staff:
+            return super().get_queryset()
+        
+        else:
+            queryset = Flight.objects.filter(date_of_departure__gt=today)
+            
+            if Flight.objects.filter(date_of_departure=today):
+                today_qs = Flight.objects.filter(date_of_departure=today).filter(etd__gt=current_time)
+
+                queryset = queryset.union(today_qs)
+            return queryset
 
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
